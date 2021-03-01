3ca019f34003572e969fe061947367421a35b277
Stephanie Wang <stephaniewang526@users.noreply.github.com>
2021-03-01 14:57:38 -0500

    Update dependencies.sh

--- .kokoro/dependencies.sh
+++ .kokoro/dependencies.sh
@@ -50,6 +50,7 @@ function completenessCheck() {
   # Output dep list generated using the flattened pom (only 'compile' and 'runtime' scopes)
   # [3/1/2021] Adding -DexcludeArtifactIds=annotations,gson,protobuf-java-util,commons-codec,commons-logging,perfmark-api
   # due to maven dependency plugin setting transitive compile/runtime deps scope to test when it is also a test dep
+  # Raised issue: https://issues.apache.org/jira/browse/MDEP-737
   msg "Generating dependency list using flattened pom..."
   mvn dependency:list -f .flattened-pom.xml -DincludeScope=runtime -DexcludeArtifactIds=annotations,gson,protobuf-java-util,commons-codec,commons-logging,perfmark-api -Dsort=true | grep '\[INFO]    .*:.*:.*:.*:.*' >.new-list.txt
 
